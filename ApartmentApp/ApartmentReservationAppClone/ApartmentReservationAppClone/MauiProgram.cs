8376368ed777bae7049b6bb08bc0a22ad327ba8a
Sile9t <eyesless1jack@gmail.com>
2024-07-19 13:48:40 +0300

    All work without crashes

--- ApartmentApp/ApartmentReservationAppClone/ApartmentReservationAppClone/MauiProgram.cs
+++ ApartmentApp/ApartmentReservationAppClone/ApartmentReservationAppClone/MauiProgram.cs
@@ -1,9 +1,37 @@
-﻿using Microsoft.Extensions.Logging;
+﻿using ApartmentReservationAppClone.DB;
+using ApartmentReservationAppClone.Services;
+using ApartmentReservationAppClone.Views;
+using ApartmentReservationAppCLone.ViewModels;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
 
 namespace ApartmentReservationAppClone
 {
     public static class MauiProgram
     {
+        public static MauiAppBuilder RegisterAppServices(this MauiAppBuilder appBuilder)
+        {
+            appBuilder.Services.AddDbContext<OccupancyContext>(
+                opt => {
+                    opt.UseSqlServer(appBuilder.Configuration
+                    .GetConnectionString("db")).LogTo(Console.WriteLine);
+                },
+                ServiceLifetime.Singleton);
+
+            appBuilder.Services.AddSingleton<IConnectivity>(Connectivity.Current);
+            appBuilder.Services.AddSingleton<IGeolocation>(Geolocation.Default);
+            appBuilder.Services.AddSingleton<IMap>(Map.Default);
+
+            appBuilder.Services.AddSingleton<ApartmentService>();
+            appBuilder.Services.AddSingleton<ApartmentsViewModel>();
+            appBuilder.Services.AddSingleton<MainPage>();
+
+            //appBuilder.Services.AddTransient<DetailsPage>();
+
+            return appBuilder;
+        }
+
         public static MauiApp CreateMauiApp()
         {
             var builder = MauiApp.CreateBuilder();
@@ -14,6 +42,7 @@ namespace ApartmentReservationAppClone
                     fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
                     fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
                 });
+            builder.RegisterAppServices();
 
 #if DEBUG
     		builder.Logging.AddDebug();
